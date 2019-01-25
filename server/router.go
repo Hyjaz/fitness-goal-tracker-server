f2f83bb1a35d2e686ba792e336311634b32bddf9
hyjaz <hyjaz.loudin@gmail.com>
2019-01-24 20:57:58 -0500

    add status check

--- server/router.go
+++ server/router.go
@@ -1,12 +1,15 @@
 package server
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/hyjaz/fitness-goal-tracker-server/controllers"
 )
 
 func initRoute() *gin.Engine {
 	r := gin.Default()
+	r.GET("/status", func(c *gin.Context) { c.JSON(http.StatusOK, gin.H{"status": "ok"}) })
 	r.GET("/", controllers.GetUser)
 	r.POST("/cycle", controllers.AddCycle)
 	r.POST("/daily", controllers.AddDaily)
