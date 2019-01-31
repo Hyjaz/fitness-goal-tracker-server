2c6d58ff8c88ee424ffeda9e88f84eb9a7ea0c06
hyjaz <hyjaz.loudin@gmail.com>
2019-01-30 20:47:56 -0500

    fix macronutrients update and remove kubernetes resource definitions

--- models/users.go
+++ models/users.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"context"
-	"log"
 
 	"github.com/hyjaz/fitness-goal-tracker-server/database"
 	"github.com/mongodb/mongo-go-driver/bson"
@@ -22,16 +21,12 @@ func getUserCollection() *mongo.Collection {
 
 // GetUser gets a user if it exists, otherwise inserts a new user and returns the document
 func GetUser(user *User) error {
-	log.Println("Inside model GetUser")
 	collection := getUserCollection()
-	log.Println("Inside model GetUser - got collection")
 	filter := bson.M{"uuid": user.UUID}
 	err := collection.FindOne(context.Background(), filter).Decode(&user)
 	if err != nil {
-		log.Println("Inside model GetUser - did not find user adding user")
 		err = addUser(user.UUID)
 		if err != nil {
-			log.Println("Inside model GetUser - could not add user")
 			return err
 		}
 		collection.FindOne(context.Background(), filter).Decode(&user)
